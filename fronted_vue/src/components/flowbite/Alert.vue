80801485961ccc223a3953b932c037a5e98eb2d3
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-03-15 18:56:55 +0630

    flowbite add

---
+++ fronted_vue/src/components/flowbite/Alert.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-300 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </div>
+</template>
--- fronted_vue/src/views/HomeView.vue
+++ fronted_vue/src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { HouseWifi } from 'lucide-vue-next';
+import Alert from '@/components/flowbite/Alert.vue';
+import Card from '@/components/flowbite/Card.vue';
 </script>
 
 <template>
   <Master>
     <p class="text-primary-100 ">
       <HouseWifi color="green" />
+      <Alert />
+      <Card />
     </p>
 
   </Master>

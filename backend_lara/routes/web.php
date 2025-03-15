ba236326d5f9f5760ff6eda6e74933f0777cacaf
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-03-15 23:21:21 +0630

    backend:laravel fortify add

--- backend_lara/routes/web.php
+++ backend_lara/routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::group(['prefix' => 'tests', ['middleware' => 'test']], function () {
+    Route::get('one', [TestController::class, 'one'])->name('tests.one');
+});
+
 require __DIR__ . '/auth.php';
+
+
